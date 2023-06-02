cd4cf9b4190e1d00000ae0f240d93c43bd3c60e4
Wins9393 <tktmmpaas@gmail.com>
2023-06-02 18:07:28 +0200

    xml structure modified

--- back/server.js
+++ back/server.js
@@ -6,17 +6,30 @@ import fastifyMultipart from "@fastify/multipart";
 import jstoxml from "jstoxml";
 import xml2js from "xml2js";
 
+let tabClientsNumber = [
+  { "BRIL 62 BAARN": 6110 },
+  { "MAYK VAN BEEK OPTIEK": 1588 },
+  { OOGDALEM: 1419 },
+  { "OOGMERK TUNDERMAN": 1073 },
+  { "OPTIEK FREIBERGER": 1193 },
+  { "'T BRILLENHUYS": 6115 },
+  { "VAN KOUWEN UW OPTICIEN": 2448 },
+  { "VON OY": 6650 },
+  { "PETER KUIPER OPTIEK ": 7548 },
+];
+
 const fastify = Fastify({
-	logger: true,
+  logger: true,
 });
 
 fastify.register(fastifyMultipart);
 
 await fastify.register(cors, {
-	origin: ["http://localhost:3000"],
-	methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-	allowedHeaders: ["Content-Type", "x-requested-with"],
-	preflightContinue: true,
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "x-requested-with", "Content-Disposition"],
+  exposedHeaders: ["Content-Disposition"],
+  preflightContinue: true,
 });
 
 const PORT = 5555;
@@ -25,229 +38,250 @@ const xmlConfig = { indent: "  " };
 const jsonFile = "./jsons/test.json";
 const parser = new xml2js.Parser();
 
+const dateNow = new Date();
+
 const convertDateFormat = (date) => {
-	const year = date.slice(0, 4);
-	const month = date.slice(4, 6);
-	const day = date.slice(6, 8);
-	return `${year}-${month}-${day}`;
+  const year = date.slice(0, 4);
+  const month = date.slice(4, 6);
+  const day = date.slice(6, 8);
+  return `${year}-${month}-${day}`;
 };
 
 const formatDateString = (date) => {
-	const year = date.substring(0, 4);
-	const month = date.substring(4, 6);
-	const day = date.substring(6, 8);
+  let dateObject;
 
-	const dateObject = new Date(year, month - 1, day);
+  const year = date.substring(0, 4);
+  const month = date.substring(4, 6);
+  const day = date.substring(6, 8);
 
-	return dateObject.toISOString();
+  if (parseInt(year) < dateNow.getFullYear()) {
+    dateObject = dateNow;
+  } else {
+    dateObject = new Date(year, month - 1, day);
+  }
+
+  return dateObject.toISOString();
+};
+
+const getClientNumber = (clientName) => {
+  let client = tabClientsNumber.find(
+    (client) => Object.keys(client)[0] === clientName
+  );
+  return client ? client[clientName] : undefined;
 };
 
 const formatJson = async (json, fields) => {
-	let parsedJson = JSON.parse(json);
+  let parsedJson = JSON.parse(json);
 
-	const fileNumber = fields.inputFileNumber.value.padStart(5, "0");
-	const testProduction = fields.testProduction.value;
+  const fileNumber = fields.inputFileNumber.value.padStart(5, "0");
+  const testProduction = fields.testProduction.value;
 
-	const supplierNumber = 11859;
-	const dateNow = new Date();
-	const dateISO = dateNow.toISOString().slice(0, 19);
-	// const dateYear = dateNow.getFullYear();
-	// const dateMonth = (dateNow.getMonth() + 1).toString();
+  const supplierNumber = 11859;
 
-	let tabInvoiceLines = [];
+  const docDate =
+    parsedJson?.["rsm:CrossIndustryInvoice"][
+      "rsm:SupplyChainTradeTransaction"
+    ][0]["ram:ApplicableHeaderTradeDelivery"][0][
+      "ram:ActualDeliverySupplyChainEvent"
+    ][0]["ram:OccurrenceDateTime"][0]["udt:DateTimeString"][0]["_"];
 
-	const docDate =
-		parsedJson?.["rsm:CrossIndustryInvoice"][
-			"rsm:SupplyChainTradeTransaction"
-		][0]["ram:ApplicableHeaderTradeDelivery"][0][
-			"ram:ActualDeliverySupplyChainEvent"
-		][0]["ram:OccurrenceDateTime"][0]["udt:DateTimeString"][0]["_"];
+  const dateYear = docDate.slice(0, 4);
+  const dateMonthTmp = docDate.slice(4, 6);
+  const dateMonth =
+    parseInt(dateYear) < dateNow.getFullYear()
+      ? String(parseInt(dateNow.getMonth()) + 1).padStart(2, "0")
+      : dateMonthTmp;
 
-	const dateYear = docDate.slice(0, 4);
-	const dateMonth = docDate.slice(4, 6);
+  let newXmlFileName =
+    dateYear < dateNow.getFullYear()
+      ? `${dateNow.getFullYear()}_${supplierNumber}${fileNumber}`
+      : `${dateYear}_${supplierNumber}${fileNumber}`;
 
-	let newXmlFileName = `${dateYear}_${supplierNumber}${fileNumber}`;
+  const supplierName =
+    parsedJson?.["rsm:CrossIndustryInvoice"][
+      "rsm:SupplyChainTradeTransaction"
+    ][0]["ram:ApplicableHeaderTradeAgreement"][0]["ram:SellerTradeParty"][0][
+      "ram:Name"
+    ][0];
 
-	const supplierName =
-		parsedJson?.["rsm:CrossIndustryInvoice"][
-			"rsm:SupplyChainTradeTransaction"
-		][0]["ram:ApplicableHeaderTradeAgreement"][0]["ram:SellerTradeParty"][0][
-			"ram:Name"
-		][0];
+  const supplierAddress =
+    parsedJson?.["rsm:CrossIndustryInvoice"][
+      "rsm:SupplyChainTradeTransaction"
+    ][0]["ram:ApplicableHeaderTradeAgreement"][0]["ram:SellerTradeParty"][0][
+      "ram:PostalTradeAddress"
+    ][0]["ram:LineOne"][0];
 
-	const supplierAddress =
-		parsedJson?.["rsm:CrossIndustryInvoice"][
-			"rsm:SupplyChainTradeTransaction"
-		][0]["ram:ApplicableHeaderTradeAgreement"][0]["ram:SellerTradeParty"][0][
-			"ram:PostalTradeAddress"
-		][0]["ram:LineOne"][0];
+  const supplierZipcode =
+    parsedJson?.["rsm:CrossIndustryInvoice"][
+      "rsm:SupplyChainTradeTransaction"
+    ][0]["ram:ApplicableHeaderTradeAgreement"][0]["ram:SellerTradeParty"][0][
+      "ram:PostalTradeAddress"
+    ][0]["ram:PostcodeCode"][0];
 
-	const supplierZipcode =
-		parsedJson?.["rsm:CrossIndustryInvoice"][
-			"rsm:SupplyChainTradeTransaction"
-		][0]["ram:ApplicableHeaderTradeAgreement"][0]["ram:SellerTradeParty"][0][
-			"ram:PostalTradeAddress"
-		][0]["ram:PostcodeCode"][0];
+  const supplierCity =
+    parsedJson?.["rsm:CrossIndustryInvoice"][
+      "rsm:SupplyChainTradeTransaction"
+    ][0]["ram:ApplicableHeaderTradeAgreement"][0]["ram:SellerTradeParty"][0][
+      "ram:PostalTradeAddress"
+    ][0]["ram:CityName"][0];
 
-	const supplierCity =
-		parsedJson?.["rsm:CrossIndustryInvoice"][
-			"rsm:SupplyChainTradeTransaction"
-		][0]["ram:ApplicableHeaderTradeAgreement"][0]["ram:SellerTradeParty"][0][
-			"ram:PostalTradeAddress"
-		][0]["ram:CityName"][0];
+  const supplierCountry =
+    parsedJson?.["rsm:CrossIndustryInvoice"][
+      "rsm:SupplyChainTradeTransaction"
+    ][0]["ram:ApplicableHeaderTradeAgreement"][0]["ram:SellerTradeParty"][0][
+      "ram:PostalTradeAddress"
+    ][0]["ram:CountryID"][0];
 
-	const supplierCountry =
-		parsedJson?.["rsm:CrossIndustryInvoice"][
-			"rsm:SupplyChainTradeTransaction"
-		][0]["ram:ApplicableHeaderTradeAgreement"][0]["ram:SellerTradeParty"][0][
-			"ram:PostalTradeAddress"
-		][0]["ram:CountryID"][0];
+  const debtorName =
+    parsedJson?.["rsm:CrossIndustryInvoice"][
+      "rsm:SupplyChainTradeTransaction"
+    ][0]["ram:ApplicableHeaderTradeAgreement"][0]["ram:BuyerTradeParty"][0][
+      "ram:Name"
+    ][0];
 
-	const debtorNumber =
-		parsedJson?.["rsm:CrossIndustryInvoice"][
-			"rsm:SupplyChainTradeTransaction"
-		][0]["ram:ApplicableHeaderTradeAgreement"][0]["ram:BuyerTradeParty"][0][
-			"ram:DefinedTradeContact"
-		][0]["ram:PersonName"][0];
+  const debtorNumber = getClientNumber(debtorName);
 
-	const invoiceAmountHT =
-		parsedJson?.["rsm:CrossIndustryInvoice"][
-			"rsm:SupplyChainTradeTransaction"
-		][0]["ram:ApplicableHeaderTradeSettlement"][0]["ram:ApplicableTradeTax"][0][
-			"ram:BasisAmount"
-		][0];
+  const invoiceAmountHT =
+    parsedJson?.["rsm:CrossIndustryInvoice"][
+      "rsm:SupplyChainTradeTransaction"
+    ][0]["ram:ApplicableHeaderTradeSettlement"][0]["ram:ApplicableTradeTax"][0][
+      "ram:BasisAmount"
+    ][0];
 
-	const invoiceNumber =
-		parsedJson?.["rsm:CrossIndustryInvoice"]["rsm:ExchangedDocument"][0][
-			"ram:ID"
-		][0];
+  const invoiceNumber =
+    parsedJson?.["rsm:CrossIndustryInvoice"]["rsm:ExchangedDocument"][0][
+      "ram:ID"
+    ][0];
 
-	const invoiceLines =
-		parsedJson?.["rsm:CrossIndustryInvoice"][
-			"rsm:SupplyChainTradeTransaction"
-		][0]["ram:IncludedSupplyChainTradeLineItem"];
+  const invoiceLines =
+    parsedJson?.["rsm:CrossIndustryInvoice"][
+      "rsm:SupplyChainTradeTransaction"
+    ][0]["ram:IncludedSupplyChainTradeLineItem"];
 
-	const createInvoiceLine = (invoiceLines) => {
-		return invoiceLines.map((line) => {
-			return {
-				invoiceline: {
-					invoiceline_articlecode_org:
-						line?.["ram:SpecifiedTradeProduct"][0]["ram:Name"][0],
-					invoiceline_amount_ex_VAT:
-						line?.["ram:SpecifiedLineTradeAgreement"][0][
-							"ram:GrossPriceProductTradePrice"
-						][0]["ram:ChargeAmount"][0],
-					invoiceline_VAT: {
-						invoiceline_VAT_percentage: 0,
-						invoiceline_VAT_amount: 0,
-					},
-					invoiceline_quantity:
-						line?.["ram:SpecifiedLineTradeAgreement"][0][
-							"ram:NetPriceProductTradePrice"
-						][0]["ram:BasisQuantity"][0],
-				},
-			};
-		});
-	};
+  const createInvoiceLine = (invoiceLines) => {
+    return invoiceLines.map((line) => {
+      return {
+        invoiceline: {
+          invoiceline_articlecode_org:
+            line?.["ram:SpecifiedTradeProduct"][0]["ram:SellerAssignedID"][0],
+          invoiceline_amount_ex_VAT:
+            line?.["ram:SpecifiedLineTradeAgreement"][0][
+              "ram:GrossPriceProductTradePrice"
+            ][0]["ram:ChargeAmount"][0],
+          invoiceline_VAT: {
+            invoiceline_VAT_percentage: 0,
+            invoiceline_VAT_amount: 0,
+          },
+          invoiceline_quantity:
+            line?.["ram:SpecifiedLineTradeAgreement"][0][
+              "ram:NetPriceProductTradePrice"
+            ][0]["ram:BasisQuantity"][0],
+        },
+      };
+    });
+  };
 
-	const content = {
-		statement: {
-			statement_information: {
-				statement_filename: `${newXmlFileName}.xml`,
-				statement_number: `${supplierNumber}${fileNumber}`,
-				statement_creationdatetime: formatDateString(docDate),
-				statement_test_production: `${testProduction
-					.slice(0, 1)
-					.toUpperCase()}`,
-				statement_year: `${dateYear}`,
-				statement_month: `${dateMonth.padStart(2, "0")}`,
-			},
-			supplier: {
-				supplier_number: `${supplierNumber}`,
-				supplier_name: `${supplierName}`,
-				supplier_address: `${supplierAddress}`,
-				supplier_postal_code: `${supplierZipcode}`,
-				supplier_city: `${supplierCity}`,
-				supplier_country: `${supplierCountry}`,
-			},
-			invoices: {
-				invoice: {
-					invoice_debtor: `${debtorNumber}`,
-					invoice_amount_ex_VAT: `${invoiceAmountHT}`,
-					invoice_VAT_amounts: {
-						invoice_VAT: {
-							invoice_VAT_percentage: 0,
-							invoice_VAT_amount: 0,
-						},
-					},
-					invoice_number: `${invoiceNumber}`,
-					invoice_date: convertDateFormat(docDate),
-					invoicelines: createInvoiceLine(invoiceLines),
-				},
-			},
-		},
-	};
+  const content = {
+    statement: {
+      statement_information: {
+        statement_filename: `${newXmlFileName}.xml`,
+        statement_number: `${supplierNumber}${fileNumber}`,
+        statement_creationdatetime: formatDateString(docDate),
+        statement_test_production: `${testProduction
+          .slice(0, 1)
+          .toUpperCase()}`,
+        statement_year: `${
+          dateYear < dateNow.getFullYear() ? dateNow.getFullYear() : dateYear
+        }`,
+        statement_month: `${dateMonth}`,
+      },
+      supplier: {
+        supplier_number: `${supplierNumber}`,
+        supplier_name: `${supplierName}`,
+        supplier_address: `${supplierAddress}`,
+        supplier_postal_code: `${supplierZipcode}`,
+        supplier_city: `${supplierCity}`,
+        supplier_country: `${supplierCountry}`,
+      },
+      invoices: {
+        invoice: {
+          invoice_debtor: `${debtorNumber}`,
+          invoice_amount_ex_VAT: `${invoiceAmountHT}`,
+          invoice_VAT_amounts: {
+            invoice_VAT: {
+              invoice_VAT_percentage: 0,
+              invoice_VAT_amount: 0,
+            },
+          },
+          invoice_number: `${invoiceNumber}`,
+          invoice_date: convertDateFormat(docDate),
+          invoicelines: createInvoiceLine(invoiceLines),
+        },
+      },
+    },
+  };
 
-	return content;
+  return { content, newXmlFileName };
 };
 
 const writeFormatJson = async (data) => {
-	try {
-		const jsonFormated = JSON.stringify(data);
-		await fs.promises.writeFile(jsonFile, jsonFormated);
-		console.log("Json file written successfully");
+  try {
+    const jsonFormated = JSON.stringify(data);
+    await fs.promises.writeFile(jsonFile, jsonFormated);
+    console.log("Json file written successfully");
 
-		return jsonFormated;
-	} catch (err) {
-		console.log("Error writing json file: ", err);
-	}
+    return jsonFormated;
+  } catch (err) {
+    console.log("Error writing json file: ", err);
+  }
 };
 
 const writeFormatXml = async (data, fields) => {
-	try {
-		// console.log("data: ", data);
-		const jsonFormated = await formatJson(data, fields);
-		const xmlFormated = toXML(jsonFormated, xmlConfig);
+  try {
+    // console.log("data: ", data);
+    const jsonFormated = await formatJson(data, fields);
+    const xmlFormated = toXML(jsonFormated.content, xmlConfig);
 
-		console.log("Xml file written successfully");
+    console.log("Xml file written successfully");
 
-		return xmlFormated;
-	} catch (err) {
-		console.log("Error writing xml file: ", err);
-	}
+    return { xmlFormated, newXmlFileName: jsonFormated.newXmlFileName };
+  } catch (err) {
+    console.log("Error writing xml file: ", err);
+  }
 };
 
 const processData = async (req, res) => {
-	try {
-		const file = await req.file();
-		const fields = await file.fields;
+  try {
+    const file = await req.file();
+    const fields = await file.fields;
 
-		const fileContent = await file.toBuffer();
-		const parsedData = await parser.parseStringPromise(fileContent.toString());
-		const jsonData = await writeFormatJson(parsedData);
-		const xmlData = await writeFormatXml(jsonData, fields);
+    const fileContent = await file.toBuffer();
+    const parsedData = await parser.parseStringPromise(fileContent.toString());
+    const jsonData = await writeFormatJson(parsedData);
+    const { xmlFormated, newXmlFileName } = await writeFormatXml(
+      jsonData,
+      fields
+    );
 
-		// const readable = Readable.from([xmlData]);
+    res.header("Content-Type", "text/xml");
+    res.header("Content-Disposition", `attachment; filename=${newXmlFileName}`);
 
-		res.header("Content-Type", "text/xml");
-		res.header("Content-Disposition", `attachment; filename=facture.xml`);
-
-		res.send(xmlData);
-	} catch (err) {
-		console.error("Failed to read file", err);
-		res.code(500).send("Erreur, veuillez rééssayer !");
-	}
+    res.send(xmlFormated);
+  } catch (err) {
+    console.error("Failed to read file", err);
+    res.code(500).send("Erreur, veuillez rééssayer !");
+  }
 };
 
 fastify.post("/api/structure-xml", processData);
 
 const start = async (PORT) => {
-	try {
-		await fastify.listen({ port: PORT });
-		console.log(`back is running on port ${PORT}`);
-	} catch (err) {
-		fastify.log.error(err);
-		process.exit(1);
-	}
+  try {
+    await fastify.listen({ port: PORT });
+    console.log(`back is running on port ${PORT}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 };
 start(PORT);
--- front/src/App.js
+++ front/src/App.js
@@ -6,127 +6,129 @@ import "./App.css";
 const { Dragger } = Upload;
 
 function App() {
-	const [inputFileNumber, setInputFileNumber] = useState(null);
-	const [testProduction, setTestProduction] = useState(null);
-	const [disableUpload, setDisableUpload] = useState(true);
+  const [inputFileNumber, setInputFileNumber] = useState(null);
+  const [testProduction, setTestProduction] = useState(null);
+  const [disableUpload, setDisableUpload] = useState(true);
 
-	useEffect(() => {
-		if (inputFileNumber !== null && testProduction !== null) {
-			setDisableUpload(false);
-		} else {
-			setDisableUpload(true);
-		}
-	}, [inputFileNumber, testProduction]);
+  useEffect(() => {
+    if (inputFileNumber !== null && testProduction !== null) {
+      setDisableUpload(false);
+    } else {
+      setDisableUpload(true);
+    }
+  }, [inputFileNumber, testProduction]);
 
-	const props = {
-		name: "file",
-		multiple: true,
-		disabled: disableUpload,
-		customRequest: async (options) => {
-			const data = new FormData();
-			data.append("file", options.file);
-			data.append("inputFileNumber", inputFileNumber);
-			data.append("testProduction", testProduction);
+  const props = {
+    name: "file",
+    multiple: true,
+    disabled: disableUpload,
+    customRequest: async (options) => {
+      const data = new FormData();
+      data.append("file", options.file);
+      data.append("inputFileNumber", inputFileNumber);
+      data.append("testProduction", testProduction);
 
-			console.log("data", data);
+      console.log("data", data);
 
-			const response = await fetch("http://localhost:5555/api/structure-xml", {
-				method: "POST",
-				body: data,
-			});
+      const response = await fetch("http://localhost:5555/api/structure-xml", {
+        method: "POST",
+        body: data,
+      });
+      const contentDisposition = response?.headers?.get("Content-Disposition");
+      const filename = contentDisposition?.split("filename=")[1];
 
-			const blob = await response.blob();
-			const url = window.URL.createObjectURL(blob);
-			const link = document.createElement("a");
-			link.href = url;
-			link.download = "facture.xml";
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = `${filename}.xml`;
 
-			document.body.appendChild(link);
+      document.body.appendChild(link);
 
-			link.click();
+      link.click();
 
-			document.body.removeChild(link);
-			window.URL.revokeObjectURL(url);
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
 
-			if (response.ok) {
-				options.onSuccess("Ok");
-			} else {
-				options.onError("Error");
-			}
-		},
-		onChange(info) {
-			const { status } = info.file;
-			if (status !== "uploading") {
-				console.log(info.file, info.fileList);
-			}
-			if (status === "done") {
-				message.success(`${info.file.name} file uploaded successfully.`);
-			} else if (status === "error") {
-				message.error(`${info.file.name} file upload failed.`);
-			}
-		},
-		onDrop(e) {
-			console.log("Dropped files", e.dataTransfer.files);
-		},
-	};
+      if (response.ok) {
+        options.onSuccess("Ok");
+      } else {
+        options.onError("Error");
+      }
+    },
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (status === "done") {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+    onDrop(e) {
+      console.log("Dropped files", e.dataTransfer.files);
+    },
+  };
 
-	const handleInputFileName = (e) => {
-		let userInputFileNumber = e.target.value;
-		const regex = /^(?!00000)[0-9]{0,5}$/;
+  const handleInputFileName = (e) => {
+    let userInputFileNumber = e.target.value;
+    const regex = /^(?!00000)[0-9]{0,5}$/;
 
-		if (regex.test(userInputFileNumber)) {
-			if (userInputFileNumber === "") {
-				setInputFileNumber(null);
-			} else {
-				setInputFileNumber(userInputFileNumber);
-			}
-		}
-	};
+    if (regex.test(userInputFileNumber)) {
+      if (userInputFileNumber === "") {
+        setInputFileNumber(null);
+      } else {
+        setInputFileNumber(userInputFileNumber);
+      }
+    }
+  };
 
-	const handleTestProduction = (e) => {
-		let userSelectTestProduction = e;
-		setTestProduction(userSelectTestProduction);
-	};
+  const handleTestProduction = (e) => {
+    let userSelectTestProduction = e;
+    setTestProduction(userSelectTestProduction);
+  };
 
-	return (
-		<div className="app">
-			<header className="app-header">
-				<div className="header-title">XML for Optitrade</div>
-			</header>
-			<section className="main">
-				<div className="doc-infos">
-					<Input
-						addonBefore="File Number"
-						value={inputFileNumber}
-						maxLength={5}
-						onChange={handleInputFileName}
-					/>
-					<Select
-						placeholder="Test / Production"
-						onChange={handleTestProduction}
-						options={[
-							{ value: "test", label: "Test" },
-							{ value: "production", label: "Production" },
-						]}
-						style={{ width: "100%" }}
-					/>
-				</div>
-				<div className="doc-upload">
-					<Dragger {...props}>
-						<p className="ant-upload-drag-icon">
-							<InboxOutlined />
-						</p>
-						<p className="ant-upload-text">
-							Click or drag file to this area to upload
-						</p>
-						<p className="ant-upload-hint">
-							Support for a single or bulk upload.
-						</p>
-					</Dragger>
-				</div>
-			</section>
-		</div>
-	);
+  return (
+    <div className="app">
+      <header className="app-header">
+        <div className="header-title">XML for Optitrade</div>
+      </header>
+      <section className="main">
+        <div className="doc-infos">
+          <Input
+            addonBefore="File Number"
+            value={inputFileNumber}
+            maxLength={5}
+            onChange={handleInputFileName}
+          />
+          <Select
+            placeholder="Test / Production"
+            onChange={handleTestProduction}
+            options={[
+              { value: "test", label: "Test" },
+              { value: "production", label: "Production" },
+            ]}
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="doc-upload">
+          <Dragger {...props}>
+            <p className="ant-upload-drag-icon">
+              <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">
+              Click or drag file to this area to upload
+            </p>
+            <p className="ant-upload-hint">
+              Support for a single or bulk upload.
+            </p>
+          </Dragger>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
